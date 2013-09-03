896bce6bba5f57eb97b7f558e36d4c13d66322ac
rg353 <rg353@georgetown.edu>
2013-09-03 12:37:13 -0400

    agilent pre-process scripts

---
+++ Agilent.preprocess.mRNA.r
@@ -0,0 +1,25 @@
+# Pre-processing script for Agilent mRNA expression data
+
+# Call libraries
+library('Agi4x44PreProcess')
+library('limma')
+
+# Set working directory
+setwd("C:/Users/xxx/")
+
+# Read expression files
+dat<-read.maimages(files=dir(), source="agilent")
+#or
+targets=read.targets(infile="targets.txt") # G4112F version
+dd=read.AgilentFE(targets,makePLOT=FALSE) # G4112F version
+
+# Background Correction & Normalization 
+ddNORM=BGandNorm(dat,BGmethod='normexp',NORMmethod='quantile',foreground='MeanSignal',background='BGMedianSignal',offset=50,makePLOTpre=FALSE,makePLOTpost=FALSE)
+
+# Summarization Correction
+
+ddPROC=summarize.probe(ddNORM,makePLOT=F, targets)
+esetPROC=build.eset(ddPROC,targets,makePLOT=F,annotation.package="hgug4112a.db")
+
+# Saving expression matrix
+write.table(exprs(esetPROC),file="dataMatrix_GSE17047.txt",sep="\t")
---
+++ Agilent.preprocess.miRNA.r
@@ -0,0 +1,31 @@
+# Pre-processing script for Agilent miRNA expression data
+
+
+# Call libraries
+library(limma)
+library(AgiMicroRna)
+
+# Set working directory
+setwd("C:/Users/xxx/")
+
+
+# Read expression files
+targets.micro=readTargets(infile="targets.txt",verbose=TRUE)
+#or
+dd=readMicroRnaAFE(targets.micro,verbose=TRUE)
+
+# Reading & Signal Extraction
+
+ddTGS.AFE = tgsMicroRna(dd,half=TRUE, makePLOT = FALSE,verbose = TRUE)  
+
+# Normalization 
+ddNORM.AFE = tgsNormalization(ddTGS.AFE, "quantile", makePLOTpre = FALSE,makePLOTpost = FALSE, targets.micro, verbose = TRUE)
+
+# Removal of Controls   
+ddPROC.AFE = filterMicroRna(ddNORM.AFE, dd,  control = TRUE,IsGeneDetected = TRUE,wellaboveNEG = FALSE, limIsGeneDetected = 75, limNEG = 25,makePLOT = FALSE, targets.micro, verbose = TRUE)
+
+# Creating eset object
+esetPROC.AFE = esetMicroRna(ddPROC.AFE, targets.micro, makePLOT = FALSE, verbose = TRUE)
+
+# Saving expression matrix
+writeEset(esetPROC.AFE, ddPROC.AFE, targets.micro, verbose = TRUE)
